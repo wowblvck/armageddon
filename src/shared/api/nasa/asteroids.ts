279a9fe59e6f31daf25d2d34258e00181e6be996
Indar Basto <indar.basto@gmail.com>
2023-08-16 20:36:23 +0300

    feat: infinite scroll

--- src/shared/api/nasa/asteroids.ts
+++ src/shared/api/nasa/asteroids.ts
@@ -10,8 +10,21 @@ export type GetAsteroidsListParams = {
   end_date?: string;
 };
 
-export const getAsteroidsList = (params: GetAsteroidsListParams): AxiosPromise<NEOFeed[]> => {
-  return apiInstance.get(FEED_URL, { params });
+export const getAsteroidsList = async (
+  params: GetAsteroidsListParams
+): Promise<NearEarthObject[]> => {
+  const { data } = await apiInstance.get<NEOFeed>(FEED_URL, { params });
+  const nearEarthObjects = Object.values(data.near_earth_objects)
+    .flat()
+    .sort((a, b) =>
+      a.close_approach_data[0].close_approach_date > b.close_approach_data[0].close_approach_date
+        ? 1
+        : a.close_approach_data[0].close_approach_date <
+          b.close_approach_data[0].close_approach_date
+        ? -1
+        : 0
+    );
+  return nearEarthObjects;
 };
 
 export const getAsteroidById = (asteroidId: string): AxiosPromise<NearEarthObject> => {
